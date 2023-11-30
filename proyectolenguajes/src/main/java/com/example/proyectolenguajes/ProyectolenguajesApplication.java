40c0e99a4dd2d949a77598b91e31c4e4351199ee
DoubtfulCafe <dvilleda2007@gmail.com>
2023-11-30 07:19:07 -0600

    comenzando

---
+++ proyectolenguajes/src/main/java/com/example/proyectolenguajes/ProyectolenguajesApplication.java
@@ -0,0 +1,13 @@
+package com.example.proyectolenguajes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectolenguajesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectolenguajesApplication.class, args);
+	}
+
+}
---
+++ proyectolenguajes/src/test/java/com/example/proyectolenguajes/ProyectolenguajesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.proyectolenguajes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectolenguajesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
