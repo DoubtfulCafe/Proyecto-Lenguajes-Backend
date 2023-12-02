7e3545f9cba271c4eb02692efebcb03161abcb67
DoubtfulCafe <dvilleda2007@gmail.com>
2023-12-02 10:50:55 -0600

    Eliminacion de librerias inecesarias

--- proyectolenguajes/src/main/java/com/example/proyectolenguajes/Modelos/AlumnoAsignatura.java
+++ proyectolenguajes/src/main/java/com/example/proyectolenguajes/Modelos/AlumnoAsignatura.java
@@ -1,14 +1,9 @@
 package com.example.proyectolenguajes.Modelos;
 
-import java.util.Date;
 
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Column;
 import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.MapsId;
