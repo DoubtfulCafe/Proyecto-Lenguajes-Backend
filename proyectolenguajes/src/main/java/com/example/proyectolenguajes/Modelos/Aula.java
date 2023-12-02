7a6833b8c886d7059719faa2b2504d8f7bd5611f
DoubtfulCafe <dvilleda2007@gmail.com>
2023-12-02 08:42:14 -0600

    avance 2

--- proyectolenguajes/src/main/java/com/example/proyectolenguajes/Modelos/Aula.java
+++ proyectolenguajes/src/main/java/com/example/proyectolenguajes/Modelos/Aula.java
@@ -1,5 +1,42 @@
 package com.example.proyectolenguajes.Modelos;
 
+import java.util.Date;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name="Aula")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Aula {
-    
+    @Id
+    @Column(name="codigocliente")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int codigoCliente;
+
+    @Column(name="nombre")
+    String nombre;
+
+    @Column(name="apellido")
+    String apellido;
+
+   
+
+   
+
+
 }
