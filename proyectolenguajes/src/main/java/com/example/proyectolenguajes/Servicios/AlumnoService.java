790fbf95ecff496db7584dc864e771dbd32a0381
DoubtfulCafe <dvilleda2007@gmail.com>
2023-12-02 14:32:56 -0600

    implementacion para obtener asignaturas

--- proyectolenguajes/src/main/java/com/example/proyectolenguajes/Servicios/AlumnoService.java
+++ proyectolenguajes/src/main/java/com/example/proyectolenguajes/Servicios/AlumnoService.java
@@ -3,6 +3,8 @@ package com.example.proyectolenguajes.Servicios;
 import java.util.List;
 
 import com.example.proyectolenguajes.Modelos.Alumno;
+import com.example.proyectolenguajes.Modelos.AlumnoAsignatura;
+import com.example.proyectolenguajes.Modelos.Asignatura;
 
 public interface AlumnoService {
     
@@ -13,5 +15,7 @@ public interface AlumnoService {
     public List<Alumno> obtenerTodos();
 
     public String actualizarAlumno (int numeroCuenta, Alumno alumno);
+
+     public List<AlumnoAsignatura> obtenerAsignaturasPorNumeroCuenta(int numeroCuenta);
 
 }
--- proyectolenguajes/src/main/java/com/example/proyectolenguajes/Servicios/Impl/AlumnoServiceImpl.java
+++ proyectolenguajes/src/main/java/com/example/proyectolenguajes/Servicios/Impl/AlumnoServiceImpl.java
@@ -1,16 +1,23 @@
 package com.example.proyectolenguajes.Servicios.Impl;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.proyectolenguajes.Modelos.Alumno;
+import com.example.proyectolenguajes.Modelos.AlumnoAsignatura;
+import com.example.proyectolenguajes.Modelos.Asignatura;
+import com.example.proyectolenguajes.Repositorios.AlumnoAsignaturaRepository;
 import com.example.proyectolenguajes.Repositorios.AlumnoRepository;
 import com.example.proyectolenguajes.Servicios.AlumnoService;
 
 @Service
 public class AlumnoServiceImpl implements AlumnoService{
+
+    @Autowired
+    private  AlumnoAsignaturaRepository alumnoAsignaturaRepository;
 
     @Autowired
     private AlumnoRepository alumnoRepository;
@@ -50,5 +57,17 @@ public class AlumnoServiceImpl implements AlumnoService{
 
         return "Alumno a actualizar no encontrado";
     }
+
     
+    public AlumnoServiceImpl(AlumnoAsignaturaRepository alumnoAsignaturaRepository) {
+        this.alumnoAsignaturaRepository = alumnoAsignaturaRepository;
+    }
+
+    @Override
+    public List<AlumnoAsignatura> obtenerAsignaturasPorNumeroCuenta(int numeroCuenta) {
+        return alumnoAsignaturaRepository.findByAlumnoNumeroCuenta(numeroCuenta);
+    }
 }
+
+  
+
