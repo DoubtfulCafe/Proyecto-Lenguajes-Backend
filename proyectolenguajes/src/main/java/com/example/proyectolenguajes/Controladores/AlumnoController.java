f9fbd21dd1305e1130236b45ea113b5a3d1c1b28
unknown <nietopavon1@gmail.com>
2023-12-02 15:50:58 -0600

    Correccion mapping de actualizacion de alumnos

--- proyectolenguajes/src/main/java/com/example/proyectolenguajes/Controladores/AlumnoController.java
+++ proyectolenguajes/src/main/java/com/example/proyectolenguajes/Controladores/AlumnoController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -37,6 +38,7 @@ public class AlumnoController {
         return this.alumnoServiceImpl.eliminarAlumno(numeroCuenta);
     }
 
+    @PutMapping("/actualizar")
     public String actualizarAlumno(@RequestParam(name = "numeroCuenta") int numeroCuenta, @RequestBody Alumno alumno){
         return this.alumnoServiceImpl.actualizarAlumno(numeroCuenta, alumno);
     }
